5255ff4d3f8a3073a3dfba1184205455cfd856f4
Pedro Cuenca <pedrocuenca95@yahoo.es>
2023-07-26 11:23:47 +0200

    Registro de personas

--- src/Controller/IndexController.php
+++ src/Controller/IndexController.php
@@ -3,9 +3,14 @@
 namespace App\Controller;
 
 use App\Entity\User;
+use Doctrine\Persistence\ManagerRegistry;
+use League\Bundle\OAuth2ServerBundle\Repository\ClientRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Routing\Annotation\Route;
+use League\Bundle\OAuth2ServerBundle\Model\Client;
 
 class IndexController extends AbstractController
 {
@@ -31,6 +36,46 @@ class IndexController extends AbstractController
         ]);
     }
 
+    
+    #[Route('/register', name: 'new_user', methods: ['POST'])]
+    public function register(Request $request, ManagerRegistry $doctrine, UserPasswordHasherInterface $passwordHasher, ClientRepository $clientRepository)
+    {
+        //el request está en json
+        
+        $request = $this->transformJsonBody($request);
+        //obtener el mail y el password del request
+        $email = $request->get('email');
+        $password = $request->get('password');
+        //crear el usuario
+        $user = new User();
+        $user->setEmail($email);
+        $user->setUuid(\Symfony\Component\Uid\Uuid::v4());
+        $user->setPassword($passwordHasher->hashPassword($user, $password));
+
+        //add user consent
+        
+        $userConsent = new \App\Entity\OAuth2UserConsent();
+        $userConsent->setUser($user);
+        $cliente = $clientRepository->getClientEntity('CerebroDev');
+        $em = $doctrine->getManager();
+        $appClient = $em->getRepository(Client::class)->findOneBy(['identifier' =>'CerebroDev']);
+        if($appClient){
+            //add cliente a user consent
+            $userConsent->setClient($appClient);
+        }
+        $userConsent->setScopes(['profile','email']);
+        $userConsent->setCreated(new \DateTimeImmutable());
+
+        $user->addOAuth2UserConsent($userConsent);
+
+        //guardar el usuario
+        $em->persist($user);
+        $em->persist($userConsent);
+        $em->flush();
+        //retornar el usuario
+        return $this->json($user);
+    }
+
     #[Route('.well-known/jwks.json', name: 'app_jwks', methods: ['GET'])]
     public function jwks(): Response
     {
@@ -51,5 +96,17 @@ class IndexController extends AbstractController
             ],
         ];
         return $this->json($jwks);
+    }
+    protected function transformJsonBody(\Symfony\Component\HttpFoundation\Request $request)
+    {
+        $data = json_decode($request->getContent(), true);
+
+        if ($data === null) {
+            return $request;
+        }
+
+        $request->request->replace($data);
+
+        return $request;
     }
 }
